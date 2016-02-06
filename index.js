3914bdb86b46f9477feb783167dae400ac965537
Alina <smaylik2410@gmail.com>
2016-02-06 20:28:26 +0200

    Refactor basket

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ xhr.addEventListener("load", function (event) {
 
     const pizzaElements = json.map(function (pizza) {
         const pizzaEl = document.createElement("div");
+        pizzaEl.dataset.id = pizza.id;
         pizzaEl.classList.add("goods-item");
         pizzaEl.innerHTML = `<h2>${pizza.name}</h2>
             <div class="image-block">
@@ -32,72 +33,62 @@ xhr.addEventListener("load", function (event) {
                 <button type="button" class="buy">Add to basket</button>
             </div>`;
 
-        pizzaEl.addEventListener("click", function(event){
-            if(event.target.classList.contains("buy")){
-                const quantity = pizzaEl.querySelector("input").value;
-                document.querySelector(".basket .cart-other").appendChild(createBasketItem(pizza, quantity));
-                document.querySelector(".total-sum").textContent = parseInt(document.querySelector(".total-sum").textContent)
-                    + pizza.price * quantity + " UAH";
-            } else if(event.target.classList.contains("minus")){
+        pizzaEl.addEventListener("click", function (event) {
+            if (event.target.classList.contains("buy")) {
+                const cartItemEl = document.querySelector(".cart-other").querySelector(`[data-id='${pizza.id}']`);
+                const quantity = Number(pizzaEl.querySelector("input").value);
+                if(!cartItemEl) {
+                    document.querySelector(".basket .cart-other").appendChild(createBasketItem(pizza, quantity));
+                    document.querySelector(".total-sum").textContent = parseInt(document.querySelector(".total-sum").textContent)
+                        + pizza.price * quantity + " UAH";
+                } else {
+                    cartItemEl.querySelector("input").value = Number(cartItemEl.querySelector("input").value) + quantity;
+                    cartItemEl.querySelector(".item-sum").textContent = pizza.price * cartItemEl.querySelector("input").value + " UAH";
+                    document.querySelector(".total-sum").textContent = parseInt(document.querySelector(".total-sum").textContent)
+                        + pizza.price * quantity + " UAH";
+                }
+            } else if (event.target.classList.contains("minus")) {
                 event.preventDefault();
                 let inputElement = pizzaEl.querySelector("input");
                 if (inputElement.value > 1) {
                     inputElement.value--;
                 }
-            } else if(event.target.classList.contains("plus")) {
+            } else if (event.target.classList.contains("plus")) {
                 event.preventDefault();
                 let inputElement = pizzaEl.querySelector("input");
                 inputElement.value++;
             }
         });
 
+
         return pizzaEl;
     });
 
-    pizzaElements.forEach(function(el){
+    pizzaElements.forEach(function (el) {
         document.querySelector(".goods-list").appendChild(el);
     });
 });
 
 
-
-
-
-
-
-
-
-
-
-
 function createBasketItem(pizza, quantity) {
     let cartItem = document.createElement("div");
     cartItem.classList.add("cart-item");
-    let itemTitle = document.createElement("span");
-    itemTitle.classList.add("item-title");
-    itemTitle.textContent = pizza.name;
-    let cartAmount = document.createElement("div");
-    cartAmount.classList.add("cart-amount");
-    let minus = document.createElement("a");
-    minus.classList.add("minus");
-    minus.href = "#";
-    minus.textContent = "-";
-    let input = document.createElement("input");
-    input.value = quantity;
-    let plus = document.createElement("a");
-    plus.classList.add("plus");
-    plus.href = "#";
-    plus.textContent = "+";
-    let itemPrice = document.createElement("span");
-    itemPrice.classList.add("item-price");
-    itemPrice.textContent = pizza.price + " UAH";
-    let itemSum = document.createElement("span");
-    itemSum.classList.add("item-sum");
-    itemSum.textContent = pizza.price * quantity + " UAH";
-    let del = document.createElement("a");
-    del.classList.add("delete");
+    cartItem.dataset.id = pizza.id;
 
-    minus.addEventListener("click", function (event) {
+    cartItem.innerHTML = `<span class="item-title">${pizza.name}</span>
+                          <div class="cart-amount">
+                              <a class="minus" href="#">-</a>
+                              <input type="text" value="${quantity}">
+                              <a class="plus" href="#">+</a>
+                          </div>
+                          <span class="item-price">${pizza.price} UAH</span>
+                          <span class="item-sum">${pizza.price * quantity} UAH</span>
+                          <a class="delete"></a>`;
+
+    const input = cartItem.querySelector("input");
+    const itemSum = cartItem.querySelector(".item-sum");
+
+    cartItem.querySelector(".minus").addEventListener("click", function (event) {
         event.preventDefault();
         if (input.value > 1) {
             input.value--;
@@ -107,7 +98,7 @@ function createBasketItem(pizza, quantity) {
         }
     });
 
-    plus.addEventListener("click", function (event) {
+    cartItem.querySelector(".plus").addEventListener("click", function (event) {
         event.preventDefault();
         input.value++;
         itemSum.textContent = pizza.price * input.value + " UAH";
@@ -115,20 +106,11 @@ function createBasketItem(pizza, quantity) {
             + Number(pizza.price) + "UAH";
     });
 
-    del.addEventListener("click", function (event) {
+    cartItem.querySelector(".delete").addEventListener("click", function (event) {
         document.querySelector(".total-sum").textContent = parseInt(document.querySelector(".total-sum").textContent) -
             input.value * pizza.price + " UAH";
         cartItem.remove();
     });
-
-    cartItem.appendChild(itemTitle);
-    cartItem.appendChild(cartAmount);
-    cartItem.appendChild(itemPrice);
-    cartItem.appendChild(itemSum);
-    cartItem.appendChild(del);
-    cartAmount.appendChild(minus);
-    cartAmount.appendChild(input);
-    cartAmount.appendChild(plus);
 
     return cartItem;
 }
